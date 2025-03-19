7bd6241c44308123bbe81fff95962a6e15ffc8d7
Хребков Данил <279015854@TOP.ACADEMY>
2025-03-19 21:11:33 +0300

    first commit

--- testing.py
+++ testing.py
@@ -1,36 +1,66 @@
-# class User:
-#     def __init__(self, id, name, active):
-#         self.id = id
-#         self.name = name
-#         self.active = active
+# # class User:
+# #     def __init__(self, id, name, active):
+# #         self.id = id
+# #         self.name = name
+# #         self.active = active
         
 
-# from dataclasses import dataclass
+# # from dataclasses import dataclass
 
-# @dataclass
-# class User:
-#     id:int
-#     name:str
-#     is_active: bool=True
+# # @dataclass
+# # class User:
+# #     id:int
+# #     name:str
+# #     is_active: bool=True
 
-# user = User(19, 'Kristofer')
-# print(user.name )
-from abc import ABC, abstractmethod
-class Worker(ABC):
-    def __init__(self, post):
-        self.post = post
-    @abstractmethod
-    def work(self):
-        print('in process...')
+# # user = User(19, 'Kristofer')
+# # print(user.name )
+# from abc import ABC, abstractmethod
+# class Worker(ABC):
+#     def __init__(self, post):
+#         self.post = post
+#     @abstractmethod
+#     def work(self):
+#         print('in process...')
 
-class BasicWorker(Worker):
-    def work(self):
-        print('Basic work in process...')
-class Director(Worker):
-    def work(self):
-        print('Not working but earning money')
+# class BasicWorker(Worker):
+#     def work(self):
+#         print('Basic work in process...')
+# class Director(Worker):
+#     def work(self):
+#         print('Not working but earning money')
 
-worker = BasicWorker('Employee')
-director = Director('Director')
-director.work()
-worker.work()
+# worker = BasicWorker('Employee')
+# director = Director('Director')
+# director.work()
+# worker.work()
+
+numbers = [
+    [
+        [
+
+        ],
+        [
+            [1]
+        ]
+    ],
+    [
+        [2],
+        [
+            [
+                [3],
+                [3.5]
+            ],
+            [4]
+        ]
+    ],
+    [5]
+]
+def print_elements(num):
+    for i in num:
+        if type(i) == list:
+            print_elements(i)
+        else:
+            print(i)
+            
+print_elements(numbers)
