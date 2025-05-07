63070419c8cab22ac9711c4f80e1de3550fa3045
Хребков Данил <279015854@TOP.ACADEMY>
2025-05-07 21:06:44 +0300

    1

--- MySiteFolder/app.py
+++ MySiteFolder/app.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template("index.html")
+from flask import Flask, jsonify, render_template
+class MyApp(Flask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setup_routes()
 
-@app.route('/login', methods=['GET', 'POST'])
+    def setup_routes(self):
+        @self.route('/')
+        def home():
+            return render_template("index.html")
+        
+        @self.route('/api/data')
+        def get_data():
+            return jsonify({"status":"ok", "data":[1,2,3]})
+app = MyApp(__name__)
+
+@app.route("/login")
 def login():
-    if request.method =="POST":
-        username = request.form['username']
-        password = request.form['password']
-        return f"Вы вошли в систему, {username}"
-    else:
-        return render_template("login.html")
-
-
+    return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+
+
+
+
+# from flask import Flask, render_template, request
+# app = Flask(__name__, static_folder="static")
+# @app.route('/')
+# def home():
+#     return render_template("index.html")
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method =="POST":
+#         username = request.form['username']
+#         password = request.form['password']
+#         return f"Вы вошли в систему, {username}"
+#     else:
+#         return render_template("login.html")
+# if __name__ == "__main__":
+#     app.run(debug=True)
