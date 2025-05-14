da786ce2966ad442ccfeb49b827462325ef21f57
Хребков Данил <279015854@TOP.ACADEMY>
2025-05-14 21:16:33 +0300

    1

--- MySiteFolder/get_app.py
+++ MySiteFolder/get_app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 
 about_title = "Страница - биография"
@@ -14,24 +13,14 @@ def main():
 def about():
     return render_template('about.html', title=about_title, langs=langs)
 
+@app.route("/opros")
+def opros():
+    return render_template("get_opros.html")
+
+@app.route("/search", methods=['GET'])
+def search():
+    query = request.args.get('q')
+    return f"Ничего не найдено по запросу: {query}"
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-# FOR GET_OPROS
-
-# from flask import Flask, request, render_template, redirect, url_for
-# app = Flask(__name__)
-
-# @app.route("/")
-# def opros():
-#     return render_template("get_opros.html")
-
-# @app.route("/search", methods=['GET'])
-# def search():
-#     query = request.args.get('q')
-#     return f"Ничего не найдено по запросу: {query}"
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
